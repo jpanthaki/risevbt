4e3ef9691192388df8140e10c303a7bd7cca1c22
Jamshed Panthaki <jpanthak@andrew.cmu.edu>
2025-04-09 09:22:26 -0400

    add comm_protocol

---
+++ RiseVBT/RiseVBT/RecordLiftView.swift
@@ -0,0 +1,18 @@
+//
+//  RecordLiftView.swift
+//  RiseVBT
+//
+//  Created by Jamshed Panthaki on 4/7/25.
+//
+
+import SwiftUI
+
+struct RecordLiftView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecordLiftView()
+}
